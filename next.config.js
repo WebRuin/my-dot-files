b483b40bf8999e9524bb898d378e6a3b81b50e63
Tim Smith <tim.smith.hdg@gmail.com>
2020-01-28 14:13:35 -0800

    first commit

---
+++ next.config.js
@@ -0,0 +1,84 @@
+const withOffline = require('next-offline')
+const withPlugins = require('next-compose-plugins')
+const optimizedImages = require('next-optimized-images')
+const webpack = require('webpack')
+require('dotenv').config()
+
+module.exports = {
+  target: 'serverless',
+}
+
+module.exports = {
+  webpack: config => {
+    const env = Object.keys(process.env).reduce((acc, curr) => {
+      acc[`process.env.${curr}`] = JSON.stringify(process.env[curr])
+      return acc
+    }, {})
+    config.plugins.push(new webpack.DefinePlugin(env))
+    return config
+  },
+}
+
+const nextConfig = {
+  target: 'serverless',
+  transformManifest: manifest => ['/'].concat(manifest), // add the homepage to the cache
+  // Trying to set NODE_ENV=production when running yarn dev causes a build-time error so we
+  // turn on the SW in dev mode so that we can actually test it
+  generateInDevMode: true,
+  workboxOpts: {
+    swDest: 'static/service-worker.js',
+    runtimeCaching: [
+      {
+        urlPattern: /^https?.*/,
+        handler: 'NetworkFirst',
+        options: {
+          cacheName: 'https-calls',
+          networkTimeoutSeconds: 15,
+          expiration: {
+            maxEntries: 150,
+            maxAgeSeconds: 30 * 24 * 60 * 60, // 1 month
+          },
+          cacheableResponse: {
+            statuses: [0, 200],
+          },
+        },
+      },
+    ],
+  },
+}
+
+module.exports = withOffline(nextConfig)
+
+module.exports = withPlugins([
+  [
+    optimizedImages,
+    {
+      // these are the default values so you don't have to provide them if they are good enough for your use-case.
+      // but you can overwrite them here with any valid value you want.
+      inlineImageLimit: 8192,
+      imagesFolder: 'images',
+      imagesName: '[name]-[hash].[ext]',
+      handleImages: ['jpeg', 'png', 'svg', 'webp', 'gif'],
+      optimizeImages: true,
+      optimizeImagesInDev: false,
+      mozjpeg: {
+        quality: 80,
+      },
+      optipng: {
+        optimizationLevel: 3,
+      },
+      pngquant: false,
+      gifsicle: {
+        interlaced: true,
+        optimizationLevel: 3,
+      },
+      svgo: {
+        // enable/disable svgo plugins here
+      },
+      webp: {
+        preset: 'default',
+        quality: 75,
+      },
+    },
+  ],
+])
